19fb351ca1dea885748cffbc69d5d19937e48fb2
moondiverc <nezzaluna10@gmail.com>
2025-11-12 14:32:23 +0700

    chore: finish tutorial 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/widgets/left_drawer.dart';
 import 'package:football_news/widgets/news_card.dart';
-import 'package:football_news/screens/newslist_form.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
